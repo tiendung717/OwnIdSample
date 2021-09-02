40b545d9f5ffa22b522503f7342c5e9c31e8ec54
Tien <tiendt8@gmail.com>
2021-09-02 21:51:59 +0700

    Update ui

--- app/src/main/java/com/ownid/sample/MainActivity.kt
+++ app/src/main/java/com/ownid/sample/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.ownid.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.lifecycle.lifecycleScope
-import com.google.firebase.FirebaseApp
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ownid.sample.databinding.ActivityMainBinding
@@ -16,22 +17,24 @@ import kotlinx.coroutines.CancellationException
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var ownIdFirebase: OwnIdFirebase
-
+    private var email = ""
     private val ownIdRegisterResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             binding.root.updateOwnIdView(result.resultCode)
             runCatching { result.data.getOwnIdResponse(result.resultCode) }
                 .onSuccess { ownIdResponse: OwnIdResponse ->
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Register success with response $ownIdResponse", Toast.LENGTH_LONG
-                    ).show()
+                    onOwnIdRegisterResponse(ownIdResponse)
+                    Log.d(
+                        TAG,
+                        "ownIdResponse data:${ownIdResponse.data} nonce: ${ownIdResponse.nonce}"
+                    )
                 }
                 .onFailure { error ->
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Register fail with error $error", Toast.LENGTH_LONG
-                    ).show()
+                    showError(error)
+                    Log.e(
+                        TAG,
+                        "fail with $error"
+                    )
                 }
         }
 
@@ -43,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater, null, false)
         setContentView(binding.root)
+
         OwnIdLogger.enabled = true
         ownIdFirebase = OwnIdFirebaseFactory.createOwnId(
             this,
@@ -52,16 +56,31 @@ class MainActivity : AppCompatActivity() {
         val registerIntent = ownIdFirebase.createRegisterIntent(this, "en", "example@gmail.com")
 
         binding.root.setOwnIdView {
-            ownIdRegisterResultLauncher.launch(registerIntent)
+            val email = binding.email.text.toString()
+            if (email.isEmpty()) {
+                binding.inputLayout.error = "Please enter email"
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.inputLayout.error = "The email is not valid"
+            } else {
+                binding.inputLayout.error = ""
+                ownIdRegisterResultLauncher.launch(registerIntent)
+            }
         }
     }
 
-    fun onOwnIdLoginResponse(ownIdResponse: OwnIdResponse) {
-        ownIdFirebase.login(ownIdResponse)
+    private fun onOwnIdRegisterResponse(ownIdResponse: OwnIdResponse) {
+        ownIdFirebase.register("Sample Name", email, ownIdResponse)
             .addOnSuccessListener {
-
+                AlertDialog.Builder(this)
+                    .setTitle("Success")
+                    .setMessage("Logged in with email $email")
+                    .show()
             }
             .addOnFailureListener { cause ->
+                Log.e(
+                    TAG,
+                    cause.message.toString()
+                )
                 when (cause) {
                     is CancellationException -> throw cause
                     is EmailAndPasswordRequired -> onOwnIdLinkOnLoginResponse(ownIdResponse)
@@ -70,13 +89,26 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    fun onOwnIdLinkOnLoginResponse(ownIdResponse: OwnIdResponse) {
-        ownIdFirebase.loginAndLink("", "", ownIdResponse)
+    private fun onOwnIdLinkOnLoginResponse(ownIdResponse: OwnIdResponse) {
+        ownIdFirebase.loginAndLink(email, PASS_SAMPLE, ownIdResponse)
             .addOnSuccessListener {
+                Log.d(
+                    TAG,
+                    "Success login with response"
+                )
             }
             .addOnFailureListener { cause ->
+                Log.e(
+                    TAG,
+                    cause.message.toString()
+                )
                 if (cause is CancellationException) throw cause
                 showError(cause)
             }
+    }
+
+    companion object {
+        const val TAG = "OwnIdSample"
+        const val PASS_SAMPLE = "1234@5678"
     }
 }
